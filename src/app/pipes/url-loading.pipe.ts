351873c0ef637d8e943d51311a5ecc6422d0fd79
Mathilde Spitzer <mathilde.spitzer@gmail.com>
2021-11-08 08:21:39 +0100

    change return value urlpipe

--- src/app/pipes/url-loading.pipe.ts
+++ src/app/pipes/url-loading.pipe.ts
@@ -6,10 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class UrlLoadingPipe implements PipeTransform {
 
   // helper function to hide url while loading
-
   transform(value:string): string {
     const urlEx = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
-    return urlEx.test(value) ? "… loading" : value;
+    return urlEx.test(value) ? "" : value;
   }
-
 }
